00bc4f8bb8a78339cbb9ad3687c5df6276511955
GUPTA SAKSHAM <c11532b@cnh1.cnhgroup.cnh.com>
2023-05-26 15:57:12 +0530

    Typo

--- middlewares/cors/index.js
+++ middlewares/cors/index.js
@@ -1,9 +1,9 @@
 const FRONTEND_LINKS = require("../../constants/frontend");
 
 function allowCORS(req, res, next) {
-  res.setHeaders("Access-Control-Allow-Origin", `${FRONTEND_LINKS.BASE_URL}`);
-  res.setHeaders("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeaders("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", `${FRONTEND_LINKS.BASE_URL}`);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
   next();
 }
 
